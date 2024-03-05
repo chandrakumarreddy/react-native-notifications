1a8b8efe6b79b3f8073e592101a8c0c1770260ec
chandra <chandrapenugonda655@gmail.com>
2024-03-05 22:38:10 +0530

    final code

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,6 +1,22 @@
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 import "../global.css";
-import { Slot } from "expo-router";
+import { Stack } from "expo-router";
+import { View } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { usePushNotifications } from "@/hooks/usePushNotifications";
 
-export default function Layout() {
-  return <Slot />;
+export default function RootLayout() {
+  const { top, bottom } = useSafeAreaInsets();
+
+  return (
+    <>
+      <StatusBar style="dark" />
+      <View
+        className="flex-1"
+        style={{ paddingTop: top, paddingBottom: bottom }}
+      >
+        <Stack screenOptions={{ headerShown: false }} />
+      </View>
+    </>
+  );
 }
